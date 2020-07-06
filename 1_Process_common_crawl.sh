3db4dc0aa1a8cd08e61961a087703a87eed24d3b
BMarcin <m1.borzymowski@gmail.com>
2020-07-06 07:26:48 +0200

    preprocessing update

--- 1_Process_common_crawl.sh
+++ 1_Process_common_crawl.sh
@@ -1,18 +1,4 @@
 python '1. Common_crawl_processing.py' \
-  --search_string=reddit.com/r/Meditation/*, \
-                  reddit.com/r/spacex/*, \
-                  forge.medium.com/*, \
-                  reddit.com/r/books/*, \
-                  reddit.com/r/harrypotter/*, \
-                  reddit.com/r/psychology/*, \
-                  reddit.com/r/geography/*, \
-                  reddit.com/r/medicine/*, \
-                  reddit.com/r/biology/*, \
-                  reddit.com/r/cancer/*, \
-                  reddit.com/r/HumanBody/*, \
-                  reddit.com/r/chemistry/*, \
-                  reddit.com/r/Inventions/*, \
-                  reddit.com/r/space/*, \
-                  reddit.com/r/technology \
-  --threads=8 \
-  --save_to=data/preprocessed/commoncrawl.txt
+  --search_string=reddit.com/r/Meditation/*,reddit.com/r/spacex/*,forge.medium.com/*,reddit.com/r/books/*,reddit.com/r/harrypotter/*,reddit.com/r/psychology/*,reddit.com/r/geography/*,reddit.com/r/medicine/*,reddit.com/r/biology/*,reddit.com/r/cancer/*,reddit.com/r/HumanBody/*,reddit.com/r/chemistry/*,reddit.com/r/Inventions/*,reddit.com/r/space/*,reddit.com/r/technology/*,reddit.com/r/AMA/*,reddit.com/r/movies/*,reddit.com/r/Coronavirus/*,cnn.com/*,washingtonpost.com/*,nejm.org/*,nature.com/*,bmj.com/*,nationalgeographic.com/*,money.cnn.com/*,kiplinger.com/*,thisismoney.co.uk/*,reddit.com/r/Physics/*,reddit.com/r/Health/*,reddit.com/r/worldnews/*,reddit.com/r/Jokes/*,reddit.com/r/copypasta/*,reddit.com/r/Economics/*,ikea.com/gb/en/*,imdb.com/*,harvard.edu/*,\
+  --threads=8\
+  --output_file_prefix=data/preprocessed/commoncrawl
