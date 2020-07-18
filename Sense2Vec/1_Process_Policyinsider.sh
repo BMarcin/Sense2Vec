09465748a4612e28cb82db8f424e94ece1da6897
BMarcin <m1.borzymowski@gmail.com>
2020-07-18 12:38:49 +0200

    New sources update

---
+++ Sense2Vec/1_Process_Policyinsider.sh
@@ -0,0 +1,5 @@
+python '1. PolicyInsiderAI_processing.py' \
+  --elastic_search_host=192.168.113.100 \
+  --elastic_search_port=9200 \
+  --temp_files_path=data/policyinsider \
+  --output_file_prefix=data/preprocessed/policyinsider
