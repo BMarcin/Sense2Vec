bad182d1a435b12456f60b74670cbf3c61213972
BMarcin <m1.borzymowski@gmail.com>
2020-07-24 23:14:58 +0200

    Custom POS tagging

--- 2. Text Corpus to Tagged Corpus.py
+++ 2. Text Corpus to Tagged Corpus.py
@@ -10,7 +10,8 @@ from Sense2Vec.tokenizer import create_custom_tokenizer as my_custom_tokenizer
 nlp = spacy.load("en_core_web_sm")
 
 
-def preprocess_pipeline(input_dir_path, output_file_path, custom_tokenizer_func, threads=4, batch=200):
+def preprocess_pipeline(input_dir_path, output_file_path, custom_tokenizer_func, replacement_list, threads=4,
+                        batch=200):
     logging.info("Reading files")
     file_inputs = [line.replace("\n", "") for file in
                    tqdm(glob(input_dir_path + "*.txt"), desc="Reading files") for
@@ -20,27 +21,31 @@ def preprocess_pipeline(input_dir_path, output_file_path, custom_tokenizer_func,
     pipe = nlp.pipe(file_inputs, disable=["ner"],
                     n_process=threads, batch_size=batch)
 
-    combined_list = []
+    # combined_list = []
     sentences = set()
 
     tokenizer = custom_tokenizer_func(nlp)
     nlp.tokenizer = tokenizer
 
     logging.info("Processing...")
-    for doc in tqdm(pipe, desc='Processing files', total=len(file_inputs)):
-        for sent in doc.sents:
-            if sent not in sentences:
-                combs = []
-                for token in sent:
-                    combs.append(token.text + "|" + token.pos_)
+    if not os.path.exists(output_file_path):
+        with open(output_file_path, 'w+', encoding='utf8') as fp:
+            for doc in tqdm(pipe, desc='Processing files', total=len(file_inputs)):
+                for sent in doc.sents:
+                    if sent not in sentences:
+                        combs = []
+                        for token in sent:
+                            combs.append(token.text + "|" + replacement_list[token.pos_])
 
-                combined_list.append(combs)
-                sentences.add(sent)
+                        fp.write(" ".join(combs) + " \n")
+                        sentences.add(sent)
+    else:
+        print(output_file_path + " Exists")
 
-    logging.info("Saving to file")
-    with open(output_file_path, 'w', encoding='utf8') as fp:
-        for sentence in combined_list:
-            fp.write(" ".join(sentence) + " \n")
+    # logging.info("Saving to file")
+    # with open(output_file_path, 'w', encoding='utf8') as fp:
+    #     for sentence in sentences:
+    #         fp.write(" ".join(sentence) + " \n")
 
 
 if __name__ == '__main__':
@@ -86,12 +91,35 @@ if __name__ == '__main__':
     logging.info("Starting preprocessing with arguments: input_dir={}, output_file_path={}, threads={}, bs={}".format(
         options.input_dir_path, options.output_file_path, options.threads, options.bs))
 
+    replacement_list = {
+        "SYM": "X",
+        "PUNCT": "PUNCT",
+        "X": "X",
+        "ADJ": "ADJ",
+        "CCONJ": "CONJ",
+        "CONJ": "CONJ",
+        "SCONJ": "CONJ",
+        "NUM": "NOUN",
+        "DET": "DET",
+        "PRON": "NOUN",
+        "ADP": "ADP",
+        "VERB": "VERB",
+        "PROPN": "NOUN",
+        "NOUN": "NOUN",
+        "PART": "X",
+        "ADV": "ADV",
+        "SPACE": "X",
+        "INTJ": "X",
+        "AUX": "AUX"
+    }
+
     preprocess_pipeline(
         options.input_dir_path,
         options.output_file_path,
         my_custom_tokenizer,
         threads=int(options.threads),
-        batch=int(options.bs)
+        batch=int(options.bs),
+        replacement_list=replacement_list
     )
 
     logging.info("Done. Log is available under: {}".format(os.path.abspath(log_file_name)))
