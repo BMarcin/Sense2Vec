daa099ffb29ed82a05841076351d7b1a16fc7d94
BMarcin <m1.borzymowski@gmail.com>
2020-08-01 11:40:30 +0200

    Processing fixes

--- 1_Process_blogs.sh
+++ 1_Process_blogs.sh
@@ -1,3 +1,3 @@
 python '1. Blogs_preprocessing.py' \
-    --input_dir_path=../_utils/blogs/ \
-    --output_file_path=data/preprocessed/blogs.txt
+  --input_dir_path=../_utils/blogs/ \
+  --output_file_prefix=data/preprocessed/blogs
